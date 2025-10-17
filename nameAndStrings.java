0975ee45d29fff089ca54ad0a30de9b437e5e753
kruz <schurzkruz882@gmail.com>
2025-10-17 03:34:53 -0400

    Refactor nameAndStrings class with comments
    Updated name and comments for clarity.

--- nameAndStrings.java
+++ nameAndStrings.java
@@ -1,8 +1,8 @@
-public class nameAndStrings {
-    public static void main(String[] args) {
-        String name = "Kruz Schurz";
-        double number = 23.4578;
-        int age = 18;
+public class nameAndStrings { //This works with print format, more to come!
+    public static void main(String[] args) { //main method
+        String name = "First Last"; // your name!
+        double number = 23.4578; // Any double or "float" number with decimal values
+        int age = 18; // Your age!
 
         String first = name.substring(0,5);
         String last = name.substring(5);
